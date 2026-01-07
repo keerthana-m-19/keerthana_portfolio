71b5f9462aa8c6d8236ecff8302057e3ed0f2860
Keerthana M <keerthanam6750@gmail.com>
2026-01-07 23:54:37 +0530

    Add files via upload

---
+++ components/Banner.js
@@ -0,0 +1,65 @@
+import { useState, useEffect, useCallback, useMemo } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+  const toRotate = useMemo(() => ["AI Engineer",
+    "ML Enthusiast",
+    "Data Analytics Explorer",
+    "Robotics Developer"], []);
+  const period = 2000;
+
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+
+  // Stable tick function
+  const tick = useCallback(() => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+
+    setText((prev) =>
+      isDeleting ? fullText.substring(0, prev.length - 1) : fullText.substring(0, prev.length + 1)
+    );
+
+    if (!isDeleting && text === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && text === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    } else {
+      setDelta(isDeleting ? 100 : 150);
+    }
+  }, [loopNum, isDeleting, text, toRotate]);
+
+  useEffect(() => {
+    const ticker = setInterval(() => tick(), delta);
+    return () => clearInterval(ticker);
+  }, [tick, delta]);
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>{`Hi! I'm Keerthana Aspiring`}<span className="wrap"> {text}</span></h1>
+            <p>
+              Passionate about Artificial Intelligence, Machine Learning,
+                    and Data Analytics, I’m currently pursuing my B.Tech in AI & DS.
+                    With hands-on projects in robotics, I love building innovative
+                    solutions that merge creativity with technology.
+            </p>
+          </Col>
+
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ components/Projects.js
@@ -0,0 +1,150 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project1.png";
+import projImg2 from "../assets/img/project2.png";
+import projImg3 from "../assets/img/project3.png";
+import colorSharp from "../assets/img/color-sharp.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Projects = () => {
+  const projects = [
+    {
+      title: "Machine Learning",
+      description: "Logistic Regression",
+      imgUrl: projImg1,
+      link: "https://github.com/keerthana-m-19/logistic_regression",
+    },
+    {
+      title: "Artificial Intelligence",
+      description: "AI Emotion Recognition",
+      imgUrl: projImg2,
+      link: "https://github.com/keerthana-m-19/AI-Emotion-recognition",
+    },
+    {
+      title: "Data Analytics",
+      description: "Netflix Analysis",
+      imgUrl: projImg3,
+      link: "https://github.com/keerthana-m-19/netflix-analysis",
+    },
+    {
+      title: "Machine Learning",
+      description: "K Nearest Neighbors",
+      imgUrl: projImg1,
+      link: "https://github.com/keerthana-m-19/KNN-Salary-Estimation",
+    },
+    {
+      title: "Computer Vision",
+      description: "Realtime yolov8",
+      imgUrl: projImg2,
+      link: "https://github.com/keerthana-m-19/realtime-yolov8",
+    },
+    {
+      title: "Data Analytics",
+      description: "Sales Data Analytics Dashboard",
+      imgUrl: projImg3,
+      link: "https://github.com/keerthana-m-19/Sales-Data-Analytics-Dashboard",
+    },
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible
+                      ? "animate__animated animate__fadeIn"
+                      : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <p>
+                    Showcasing my journey of creativity and innovation.
+                    <br />
+                    From interactive web applications to intelligent AI
+                    solutions, these projects highlight my skills,
+                    passion, and problem-solving expertise.
+                  </p>
+
+                  <Tab.Container
+                    id="projects-tabs"
+                    defaultActiveKey="first"
+                  >
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible
+                          ? "animate__animated animate__slideInUp"
+                          : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                              />
+                            );
+                          })}
+                        </Row>
+                      </Tab.Pane>
+
+                      <Tab.Pane eventKey="second">
+                        <p>
+                          This project demonstrates my ability to
+                          solve complex problems and build efficient
+                          solution. From planning to execution, I
+                          ensure every detail reflects quality,
+                          innovation, and functionality.
+                        </p>
+                      </Tab.Pane>
+
+                      <Tab.Pane eventKey="third">
+                        <p>
+                          This project highlights my expertise in
+                          creating innovative solutions and seamless
+                          user experiences. Every feature is crafted
+                          to combine functionality, efficiency, and
+                          aesthetic appeal.
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* ✔ Added alt tag for accessibility */}
+      <img
+        className="background-image-left"
+        src={colorSharp}
+        alt=""
+      />
+    </section>
+  );
+};
